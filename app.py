7a198a249c60680695eb0219a11001b7dcf1931c
malo-romano <malo.romano@gmail.com>
2023-02-14 20:21:15 +0100

    Run every hour

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ def job():
 
 
 
-schedule.every(1).seconds.do(job)
+schedule.every(1).hours.do(job)
 print("Welcome to PostcardCreatorBot.\n\nThis script will be executed periodically, every hour.")
 while True:
     schedule.run_pending()
