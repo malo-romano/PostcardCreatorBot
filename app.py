69f7912a093bf2761ff2937db9ee0d69889dc222
malo-romano <malo.romano@gmail.com>
2023-02-14 16:33:06 +0100

    Card sending is working

--- app.py
+++ app.py
@@ -5,26 +5,29 @@ import json
 from PIL import Image
 from postcard_creator.postcard_creator import PostcardCreator, Postcard, Token, Recipient, Sender
 
-directory = "appdata/images"
-useddirectory = directory + r"\used";
+directory = "/usr/pccb/appdata/images"
+useddirectory = directory + r"/used";
 
-config = json.load(open("appdata/configuration.json"))
+config = json.load(open("/usr/pccb/appdata/configuration.json"))
+
 
 def job():
     # Connexion
     token = Token()
+    print("===============================================================")
     print("Hi ! First of all, I will try to connect using your credentials")
 
     try:
         token.fetch_token(username=config['username'], password=config['password'], method=config['loginmethod'])
-        #token.has_valid_credentials(username=config['username'], password=config['password'])
+        # token.has_valid_credentials(username=config['username'], password=config['password'])
         print("Yaaasss I succeed")
     except Exception:
-        print("Sorry, an error occured while logging you in.\nCheck your username/password\nIf it still does not work, big L for you")
+        print(
+            "Sorry, an error occured while logging you in.\nCheck your username/password\nIf it still does not work, big L for you")
         exit()
         return
 
-    # Test postcard gratuite dispo ou non
+    # Does the user has a free postcard available ?
     try:
         w = PostcardCreator(token)
         if not w.has_free_postcard():
@@ -32,7 +35,6 @@ def job():
             return
     except Exception:
         print("Huuuum, a strange error occured, you should try to BURN YOUR COMPUTER")
-
 
     print("I'm now searching for available picture to send")
     # Parcours des fichiers dans le répertoire
@@ -43,13 +45,45 @@ def job():
             print(f"I found one ! ({filename})")
             print(f"Full path is : {filepath}")
             print("I will now try to send the image")
+
+            cardWasSent = False
+
             try:
-                #SEND IMAGE
+                recipient = Recipient(
+                    prename=config['recipient']['firstname'],
+                    lastname=config['recipient']['lastname'],
+                    street=config['recipient']['street'],
+                    place=config['recipient']['city'],
+                    zip_code=config['recipient']['npa'])
+
+                sender = Sender(
+                    prename=config['sender']['firstname'],
+                    lastname=config['sender']['lastname'],
+                    street=config['sender']['street'],
+                    place=config['sender']['city'],
+                    zip_code=config['sender']['npa'])
+
+                initialPic = Image.open(filepath)
+
+                initialPic.close()
+                cardPicture = open(filepath, 'rb')
+                card = Postcard(
+                    message=config['card']['message'],
+                    recipient=recipient,
+                    sender=sender,
+                    picture_stream=cardPicture)
+
+                cardWasSent = w.send_free_card(postcard=card, mock_send=False, image_export=False)
+                cardPicture.close()
                 os.rename(filepath, usedfilepath)
             except Exception:
                 print("I'm really bad... I was not able to send your postcard, I will try again in one hour !")
-
-            return
+            if cardWasSent:
+                print("Card was sent")
+                exit()
+                return
+            else:
+                print('Card was not sent, trying another...')
 
 
 
