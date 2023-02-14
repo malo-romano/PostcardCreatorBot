9fc4fcb56536d49b8ccef780e67f2438e7be2790
malo-romano <malo.romano@gmail.com>
2023-02-14 20:05:26 +0100

    Running batch

---
+++ RUN.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+
+SET scriptname=%~d0%~p0appdata
+ECHO "%scriptname%"
+
+docker build -t pccb .
+docker run -t -d -v %scriptname%:/usr/pccb/appdata pccb
+
+echo Done. Container is now running in docker.
+
+PAUSE
