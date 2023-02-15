70af44d6acbec53ad4f95e4fd3bd2c1b436b1764
malo-romano <malo.romano@gmail.com>
2023-02-15 10:01:33 +0100

    Error catching for image resizing

--- postcardmanager.py
+++ postcardmanager.py
@@ -90,26 +90,29 @@ def send_postcard(w, config, filepath, usedfilepath):
         return False
 
 def resize_image(filepath):
-    image = Image.open(filepath)
-    width = image.size[0]
-    height = image.size[1]
-    landscape = True if width > height else False
-    minwidth = 1819 if landscape else 1311
-    minheight = 1311 if landscape else 1819
-    ratioWE = width / height
-    ratioEW = height / width
-    if width < minwidth or height < minheight:
-        print("Image too small, resizing it")
-        newwidth = 0
-        newheight = 0
-        if width < minwidth:
-            newwidth = minwidth
-            newheight = minwidth * ratioEW
-        if newheight < minheight:
-            newheight = minheight
-            newwidth = minheight * ratioWE
+    try:
+        image = Image.open(filepath)
+        width = image.size[0]
+        height = image.size[1]
+        landscape = True if width > height else False
+        minwidth = 1819 if landscape else 1311
+        minheight = 1311 if landscape else 1819
+        ratioWE = width / height
+        ratioEW = height / width
+        if width < minwidth or height < minheight:
+            print("Image too small, resizing it")
+            newwidth = 0
+            newheight = 0
+            if width < minwidth:
+                newwidth = minwidth
+                newheight = minwidth * ratioEW
+            if newheight < minheight:
+                newheight = minheight
+                newwidth = minheight * ratioWE
 
-        image_resized = image.resize((int(newwidth), int(newheight)))
-        image.close()
-        image_resized.save(filepath)
-        image_resized.close()
+            image_resized = image.resize((int(newwidth), int(newheight)))
+            image.close()
+            image_resized.save(filepath)
+            image_resized.close()
+    except Exception as e:
+        print("Error while resizing the image : " + str(e))
